60ffede43718efb0635961ed55af092bc9aa14dc
Xufei Zhao <xzhao@Xufeis-MacBook-Pro.local>
2018-03-04 15:31:34 -0800

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
+
+//Assigns setting name 'view engine' to value 'hbs'
+//'hbs' will be the default engine extension to use when omitted.
 app.set('view engine', 'hbs');
 
+//app.use([path,] callback [, callback...])
+//path defaults to '/' so the following will be mounted to root
+//and executed for every request to the app
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`
@@ -19,6 +27,8 @@ app.use((req, res, next) => {
   next();
 });
 
+//without "next()", this middleware will not
+//allow the request to go beyond it
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
@@ -33,6 +43,10 @@ hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
+//app.get(path, callback): Routes HTTP GET requests to the
+//specified path with the specified callback functions.
+//res.render(view [, locals] [, callback]): Renders a view
+//and sends the rendered HTML string to the client.
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home page',
@@ -46,12 +60,16 @@ app.get('/about', (req, res) => {
   });
 });
 
+//res.send([body]): Sends the HTTP response.
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+//app.listen(port, [hostname], [backlog], [callback]): Binds and
+//listens for connections on the specified host and port.
+//This method is identical to Node’s http.Server.listen().
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
